9babc191334cbbfb44addb0c28f6821467535274
Rio Bayu Sentosa <riobayusentosa@gmail.com>
2022-04-21 12:51:56 +0700

    stream file

--- 15-create-big-file.js
+++ 15-create-big-file.js
@@ -1,6 +1,6 @@
 const {writeFileSync} = require('fs');
 
-for (let i = 0; i < 100000; i++) {
+for (let i = 0; i < 10000000; i++) {
     writeFileSync('./content/big.txt', `Selamat Datang ${i}\n`, {flag : 'a'})
     
 }
